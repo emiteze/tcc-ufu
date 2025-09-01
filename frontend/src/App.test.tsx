24cb672029829850004c2e306b0ef25a463818c6
Mateus Henrique Alves da Cruz <emiteze@hotmail.com>
2025-09-01 17:44:28 -0300

    Fixing unit tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,12 +4,12 @@ import App from './App';
 
 test('renders customer management system', () => {
   render(<App />);
-  const headerElement = screen.getByText(/customer management system/i);
+  const headerElement = screen.getByText(/sistema de gestão de clientes/i);
   expect(headerElement).toBeInTheDocument();
 });
 
 test('renders add new customer button', () => {
   render(<App />);
-  const buttonElement = screen.getByText(/add new customer/i);
+  const buttonElement = screen.getByText(/adicionar novo cliente/i);
   expect(buttonElement).toBeInTheDocument();
 });
